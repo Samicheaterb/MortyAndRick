1b9fa8c6f6247f8b3eb107322a9be58b632abf66
Samich <sam.bruce@stewart.com>
2023-02-01 08:28:34 -0600

    Initial Commit

---
+++ MortyAndRick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortyAndRick
+//
+//  Created by Sam Bruce on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
